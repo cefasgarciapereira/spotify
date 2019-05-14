421ef648b6057357a7dd6ac8a310cfd924a00fcf
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2019-05-14 19:50:25 -0300

    event on button

--- src/components/GreenButton.js
+++ src/components/GreenButton.js
@@ -5,12 +5,17 @@ export default class GreenButton extends React.Component {
 
   constructor(props){
     super(props);
+    // This binding is necessary to make `this` work in the callback
+    this.togglePremium = this.togglePremium.bind(this);
+  }
 
+  togglePremium(){
+    alert("Premium!");
   }
 
     render() {
         return (
-          <button className="green-button">{this.props.text}</button>
+          <button className="green-button" onClick={this.togglePremium}>{this.props.text}</button>
         )
     }
 }
