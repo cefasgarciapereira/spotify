bb9a223c1abcd9032ef80e934387c99e564d1d34
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2019-05-14 00:04:28 -0300

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import '../assets/css/hero.css';
 
 export default class Hero extends React.Component {
 
     render() {
         return (
-            <div>
-              Body!!!
+            <div className="hero">
+              <h1 className="title">Música para todos.</h1>
+              <h2 className="sub-title">Milhões de músicas à sua escolha. E nem precisa de cartão de crédito.</h2>
+              <button className="round-button">Obtenha o Spotify Free</button>
             </div>
         )
     }
